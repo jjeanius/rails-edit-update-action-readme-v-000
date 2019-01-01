e111ff5aa67421e097ad64363873f6e2571d0a4a
Judy McGoldrick <jjeanius@verizon.net>
2019-01-01 00:21:07 +0000

    Automatically backed up by Learn

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,7 +24,7 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])
     @article.update(title: params[:title], description: params[:description])
-    redirect_to @article
+    redirect_to article_path(@article)
   end
 
   def show
